745916bdfeb44cbaf96253fd357b0e99464eac26
poholo502 <poholo502@gmail.com>
2019-05-10 17:22:41 -0500

    finishing documentation

--- src/main/java/com/java/EurekaServer.java
+++ src/main/java/com/java/EurekaServer.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 
+ * @author Poho
+ * This class is to register the EurekaServer so we can have other services register themselves as clients on it
+ * 
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServer {
